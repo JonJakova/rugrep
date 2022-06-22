63ed995298a3851324294113df7e7258dcae6264
Jon Jakova <jon.jakova@gmail.com>
2022-06-22 12:43:41 +0200

    feat: stderr

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use rugrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|_| {
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     
-    if let Err(_) = rugrep::run(config) {
+    if let Err(e) = rugrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
